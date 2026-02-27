4aa30d87066fb65faeb31d11c175ff8564e674d1
Fatonim <157877800+Fatonim@users.noreply.github.com>
2026-02-27 11:06:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-TOKEN = '6353821133:AAHFFi_lNKpb2fjPt-j9Rqa8F5uZpXxfhWg'
+TOKEN = 'secret'
 URL_PROBLEMSET = 'https://codeforces.com/problemset?tags='
+
 MSG_ERROR = "Something went wrong, try again."
